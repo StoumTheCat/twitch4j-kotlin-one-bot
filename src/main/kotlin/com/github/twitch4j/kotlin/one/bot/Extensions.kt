876fd748c45a43f4ab62a86eb1cdb8c0a0badfcd
Andrey Tkach <tkachaa1@gmail.com>
2021-12-19 21:13:19 +0400

    Extensions for stuff twitchList - lists items with delimeter ChannelMessageEvent extensions for command, message without command and split argumets from messages

--- src/main/kotlin/com/github/twitch4j/kotlin/one/bot/Extensions.kt
+++ src/main/kotlin/com/github/twitch4j/kotlin/one/bot/Extensions.kt
@@ -1,7 +1,20 @@
 package com.github.twitch4j.kotlin.one.bot
 
-fun <T> Iterable<T>.toTwitchList(): String {
-    return "⠤".repeat(30).plus(this.map { it.toString() }.reduce { prev, next ->
-        "$prev ${"⠤".repeat(30)} $next"
-    })
-}
+import com.github.twitch4j.chat.events.channel.ChannelMessageEvent
+
+val <T> Iterable<T>.twitchList: String
+    get() {
+        return "⠤".repeat(30).plus(this.map { it.toString() }.reduce { prev, next ->
+            "$prev ${"⠤".repeat(30)} $next"
+        })
+    }
+
+val ChannelMessageEvent.messageWithoutCommand: String
+    get() = this.message.substringAfter(" ")
+
+val ChannelMessageEvent.messageArguments: List<String>
+    get() = this.messageWithoutCommand.split(Regex("""\s*\|\s*"""))
+
+val ChannelMessageEvent.command: String?
+    get() = if (this.message.startsWith("!")) this.message.substringBefore(" ").removePrefix("!")
+    else null
